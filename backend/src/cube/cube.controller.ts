0e37d7b4dd58ee0afd1ded81a9f7ea6b1974896d
avet-davtyan <avetdavtyan04@gmail.com>
2024-03-23 09:15:11 +0400

    add test cube api call

--- backend/src/cube/cube.controller.ts
+++ backend/src/cube/cube.controller.ts
@@ -1,44 +1,56 @@
-import { Controller, Post, Body, UseInterceptors, UploadedFiles } from '@nestjs/common';
-import { FileFieldsInterceptor } from '@nestjs/platform-express';
-import * as fs from 'fs';
-import { join } from 'path';
-import { Request } from 'express';
+import {
+  Controller,
+  Post,
+  Body,
+  UseInterceptors,
+  UploadedFiles,
+  Get,
+} from "@nestjs/common";
+import { FileFieldsInterceptor } from "@nestjs/platform-express";
+import * as fs from "fs";
+import { join } from "path";
+import { Request } from "express";
 
-@Controller('cube')
+@Controller("cube")
 export class CubeController {
-	@Post()
-	@UseInterceptors(
-		FileFieldsInterceptor([
-			{ name: 'image1', maxCount: 1 },
-			{ name: 'image2', maxCount: 1 },
-			{ name: 'image3', maxCount: 1 },
-			{ name: 'image4', maxCount: 1 },
-			{ name: 'image5', maxCount: 1 },
-			{ name: 'image6', maxCount: 1 },
-		]),
-	)
-	async uploadFile(@UploadedFiles() files, @Body() userData: any) {
-		const image1 = files['image1'][0];
-		console.log(image1);
-		try {
-			// Create a random directory name
-			const randomDirectoryName = 'cube_images/' + 'avet';
-			fs.mkdirSync(randomDirectoryName, { recursive: true });
+  @Get()
+  get() {
+    return "cubes";
+  }
 
-			const randomFileName = Math.random().toString(36).substring(7) + '.jpg';
+  @Post()
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      { name: "image1", maxCount: 1 },
+      { name: "image2", maxCount: 1 },
+      { name: "image3", maxCount: 1 },
+      { name: "image4", maxCount: 1 },
+      { name: "image5", maxCount: 1 },
+      { name: "image6", maxCount: 1 },
+    ])
+  )
+  async uploadFile(@UploadedFiles() files, @Body() userData: any) {
+    const image1 = files["image1"][0];
+    console.log(image1);
+    try {
+      // Create a random directory name
+      const randomDirectoryName = "cube_images/" + "avet";
+      fs.mkdirSync(randomDirectoryName, { recursive: true });
 
-			// Construct the full path to save the image
-			const imagePath = join(randomDirectoryName, randomFileName);
+      const randomFileName = Math.random().toString(36).substring(7) + ".jpg";
 
-			// Write the image buffer to the file
-			fs.writeFile(imagePath, image1.buffer, () => {
-				console.log(imagePath);
-			});
+      // Construct the full path to save the image
+      const imagePath = join(randomDirectoryName, randomFileName);
 
-			return imagePath;
-		} catch (error) {
-			// Handle error appropriately
-			console.log(error);
-		}
-	}
+      // Write the image buffer to the file
+      fs.writeFile(imagePath, image1.buffer, () => {
+        console.log(imagePath);
+      });
+
+      return imagePath;
+    } catch (error) {
+      // Handle error appropriately
+      console.log(error);
+    }
+  }
 }
