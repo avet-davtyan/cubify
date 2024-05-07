130267e1287f0c8bf7ef258c663012e4d5444bb8
avet-davtyan <avetdavtyan04@gmail.com>
2024-05-07 15:58:33 +0400

    add test for CubeController

---
+++ backend/src/cube/pipes/validate-cube-param/validate-cube-param.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { ValidateCubeParamPipe } from './validate-cube-param.pipe';
+
+describe('ValidateCubeParamPipe', () => {
+  it('should be defined', () => {
+    expect(new ValidateCubeParamPipe()).toBeDefined();
+  });
+});
