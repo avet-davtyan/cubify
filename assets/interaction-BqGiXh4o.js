89e17ac024819134657a025ac5d8009e5b4c2e83
avet-davtyan <avetdavtyan04@gmail.com>
2024-08-16 17:28:49 +0400

    Updates

--- assets/interaction-BqGiXh4o.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/Linker-CPMcxgYc.js","assets/Ranges-FkYEyE5B.js","assets/index-DnB0oRlV.js","assets/index-kB1-Xki0.css","assets/index-m6MNoYsZ.js","assets/OptionsColor-Dgdczrtd.js","assets/ParticlesInteractorBase-vfDeBun3.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as i}from"./index-DnB0oRlV.js";async function o(t,r=!0){await t.addInteractor("particlesLinks",async a=>{const{Linker:n}=await i(()=>import("./Linker-CPMcxgYc.js"),__vite__mapDeps([0,1,2,3,4,5,6]));return new n(a)},r)}export{o as loadLinksInteraction};
